70bf65dd1987f06953fb70496710e2dbe0a5a28c
Israel Borba <israel.borba@totvs.com.br>
2020-03-06 17:11:37 -0300

    installing jest-express

---
+++ __tests__/middlewares/validators/headers.test.ts
@@ -0,0 +1,10 @@
+import { Request } from 'jest-express/lib/request'
+import { headerValidator } from '../../../src/middlewares/validators/headers'
+
+describe('headerValidator middleware', () => {
+  test('should validate a token', () => {
+  })
+
+  test('should return bad request if not provided token', () => {
+  })
+})
